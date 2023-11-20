abae6e2ca3968f9346494140909a714c15b977c6
IoanSavt <giannhssav@hotmail.com>
2023-11-21 00:22:23 +0200

    Places

---
+++ app/src/main/java/com/example/tourguide/Places.java
@@ -0,0 +1,54 @@
+package com.example.tourguide;
+
+public class Places {
+
+    String name, description;
+    double latitude, longitude;
+
+    public Places(){}
+
+    public Places(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Places(String name, String description, double latitude, double longitude) {
+        this.name = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longtitude) {
+        this.longitude = longtitude;
+    }
+}
